37ea10476a4de134d71344884732599bec8a2eea
Antony Macchiato <antonymacchiato1@gmail.com>
2026-02-10 16:26:58 +0200

    Update LiquidityPool.sol

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
-interface IERC20Like {
+interface IERC20Like { 
     function balanceOf(address) external view returns (uint256);
 }
 
