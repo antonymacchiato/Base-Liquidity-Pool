2ad01c0e4263972ab07ca47fb58fdfa8c151c848
Antony Macchiato <antonymacchiato1@gmail.com>
2026-03-17 16:42:32 +0200

    Update LiquidityPool.sol

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract LiquidityPool {
+contract LiquidityPool { 
     address public token0;
     address public token1;
 
